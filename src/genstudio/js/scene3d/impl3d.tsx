1c46376512fd13af93c9a0dcd2bd05c7e4158b13
Matthew Huebert <mhuebert@gmail.com>
2025-03-04 16:37:22 +0100

    remove clearcolor red

--- src/genstudio/js/scene3d/impl3d.tsx
+++ src/genstudio/js/scene3d/impl3d.tsx
@@ -226,8 +226,7 @@ function renderPass({
   const pass = cmd.beginRenderPass({
     colorAttachments: [{
       view: context.getCurrentTexture().createView(),
-      // clearValue: { r: 0, g: 0, b: 0, a: 1 },
-      clearValue: { r: 1, g: 0, b: 0, a: 1}, // temporarily set red bg
+      clearValue: { r: 0, g: 0, b: 0, a: 1 },
       loadOp: 'clear',
       storeOp: 'store'
     }],
